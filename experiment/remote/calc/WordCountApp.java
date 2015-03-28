8277d9885137a60c492832e3bd5da9cb51c5a084
hua.zhang <hua.zhang@email.wsu.edu>
2015-03-28 07:53:36 -0700

    able to run in concurrenct

--- experiment/remote/calc/WordCountApp.java
+++ experiment/remote/calc/WordCountApp.java
@@ -49,17 +49,60 @@ public class WordCountApp {
 	public static void main(String[] args) throws Exception {
 		
 		String fileName = "Othello.txt";
-		if(args[0] != null) {
-			System.out.println(args[0] + "");
-			fileName = args[0];
-		}
-		// start wordcount application 
-		startWordCountApp(args[0]);
 		
-	
+		 if (args.length == 0 || args[0].equals("book8.txt")){
+     		startWordCountApp(args[0]); // // start wordcount application
+		}
+		if(args.length == 0 ||  args[0].equals("book7.txt")) {
+//		      startRemoteCalculatorSystem();
+//		      startRemoteLookupSystem();
+			startWordCountApp(args[0]); // // start wordcount application
+		}
 	}
+	
+	 public static void startRemoteCalculatorSystem() {
+		    final ActorSystem system = ActorSystem.create("CalculatorSystem",
+		        ConfigFactory.load(("calculator")));
+		    system.actorOf(Props.create(CalculatorActor.class), "calculator1");
+		    system.actorOf(Props.create(CalculatorActor.class), "calculator2");
+		    System.out.println("Started CalculatorSystem");
+		  }
 
+		  public static void startRemoteLookupSystem() {
+
+		    final ActorSystem system = ActorSystem.create("LookupSystem",
+		        ConfigFactory.load("remotelookup"));
+		    final String path1 = "akka.tcp://CalculatorSystem@ecs301c-7.labs.encs:2752/user/calculator1";
+		    final String path2 = "akka.tcp://CalculatorSystem@ecs301c-7.labs.encs:2752/user/calculator2";
+		    
+		    final ActorRef actor1 = system.actorOf(
+		    		Props.create(LookupActor.class, path1), "lookupActor1");
+
+		    final ActorRef actor2 = system.actorOf(
+		    		Props.create(LookupActor.class, path1), "lookupActor2");
+		    
+		    System.out.println("Started LookupSystem");
+		    final Random r = new Random();
+		    system.scheduler().schedule(Duration.create(1, SECONDS),
+		        Duration.create(1, SECONDS), new Runnable() {
+		          @Override
+		          public void run() {
+		            if (r.nextInt(100) % 2 == 0) {
+		              actor1.tell(new Op.Add(r.nextInt(100), r.nextInt(100)), null);
+		              actor2.tell(new Op.Add(r.nextInt(100), r.nextInt(100)), null);
+		            } else {
+		              actor1.tell(new Op.Subtract(r.nextInt(100), r.nextInt(100)), null);
+		             actor2.tell(new Op.Subtract(r.nextInt(100), r.nextInt(100)), null);
+		            }
+
+		          }
+		        }, system.dispatcher());
+
+		  }
+	
+	
 	public static void startWordCountApp(String fileName)
+	
 			throws Exception {
 
 		long start;
@@ -68,7 +111,7 @@ public class WordCountApp {
 				TimeUnit.SECONDS));
 
 		final ActorSystem actorSystem = ActorSystem.create(
-				"MapReduceAppSystem", ConfigFactory.load(("metaActor")));
+				"MapReduceAppSystem"+fileName.substring(4,5), ConfigFactory.load(("metaActor" + fileName.substring(4,5))));
 
 		Thread.sleep(5000);
 		start = System.currentTimeMillis();
@@ -108,8 +151,8 @@ public class WordCountApp {
 				.println(String
 						.format("\n\tClientActor estimate: \t\t\n\tCalculation time: \t"
 								+ duration));
-		if (result != null)
-			actorSystem.shutdown();
+//		if (result != null)
+//			actorSystem.shutdown();
 
 	}
 
